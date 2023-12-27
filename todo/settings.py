632e7697b34c8f0ca916053197b44df4f2b85e8d
Harsh77480 <otherworks77480@gmail.com>
2023-12-27 17:14:49 +0530

    testing

--- todo/settings.py
+++ todo/settings.py
@@ -148,7 +148,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "tech77480@gmail.com"
-EMAIL_HOST_PASSWORD = "rbvn owqg gnco ksjb"
+EMAIL_HOST_PASSWORD = "hitesh"
 
 from datetime import datetime, timedelta
 SIMPLE_JWT = {
